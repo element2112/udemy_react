213414cbbb5800415de2b5898c7df5e2eeecd5a2
Brittanie <bs009@live.com>
2020-10-03 16:16:33 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
+import About from './components/pages/About';
+import User from './components/users/User';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import axios from 'axios'
 import './App.css';
 
@@ -10,6 +13,7 @@ class App extends Component {
 
   state = {
     users: [],
+    user: {},
     loading: false,
     alert: null
   }
@@ -35,6 +39,15 @@ class App extends Component {
     this.setState({ users: res.data.items, loading: false });
   }
 
+  // get single user
+  getUser = async (username) => {
+
+    this.setState({ loading: true });
+    const res = await axios.get(`https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+    this.setState({ user: res.data, loading: false });
+  }
+
   // clear users from state
   clearUsers = () => {
     this.setState({ users:[], loading: false })
@@ -48,22 +61,35 @@ class App extends Component {
   }
 
   render() {
-    const { users, loading } = this.state;
+    const { users, loading, user } = this.state;
 
     return (
-      <div className='App'>
-        <Navbar title="Github Finder" />
-        <div className="container">
-          <Alert alert={this.state.alert} />
-          <Search 
-            searchUsers={this.searchUsers} 
-            clearUsers={this.clearUsers} 
-            showClear={  users.length > 0 ? true : false } 
-            setAlert={this.setAlert}
-            />
-          <Users loading={loading} users={users} />
+      <Router>
+        <div className='App'>
+          <Navbar title="Github Finder" />
+          <div className="container">
+            <Alert alert={this.state.alert} />
+            <Switch>
+              <Route exact path='/' render={props => (
+                <Fragment>
+                  <Search 
+                    searchUsers={this.searchUsers} 
+                    clearUsers={this.clearUsers} 
+                    showClear={  users.length > 0 ? true : false } 
+                    setAlert={this.setAlert}
+                    />
+                  <Users loading={loading} users={users} />
+                </Fragment>
+                )} 
+              />
+              <Route exact path='/about' component={About} />
+              <Route exact path='/user/:login' render={props => (
+                <User { ...props } getUser={this.getUser} user={user} loading={loading} />
+              )}/>
+            </Switch>
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 
 const Navbar = ({ title }) => {
@@ -8,6 +9,15 @@ const Navbar = ({ title }) => {
         <h1>
           {title}
         </h1>
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/about'>About</Link> 
+            {/*link keeps the state when switching pages */}
+          </li>
+        </ul>
       </div>
     )
 }
